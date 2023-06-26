c0bd1ec1bbe95321bb67080304bd9c6854d5c19e
なるみ <toucans-cutouts0f@icloud.com>
2023-06-26 22:43:40 +0800

    feat: integrate ConversationalRetrievalChain for chatbot functionality
    - Add a new file `agent.py`
    - Import necessary libraries at the top of the file
    - Load data from a JSON file
    - Split the data into documents using a recursive character text splitter
    - Generate a vectorstore using Chroma and OpenAIEmbeddings
    - Create a ConversationalRetrievalChain from a ChatOpenAI model and the generated vectorstore
    - Enable verbose mode for the ConversationalRetrievalChain
    - Run the chain in a while loop to interact with the user input

---
+++ agent.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+from maicoinfaqbot.utils import load_json
+
+
+def main():
+    load_dotenv()
+
+    data = load_json('data/maicoin_faq_zh.json')
+
+    docs = RecursiveCharacterTextSplitter().split_documents(
+        [Document(page_content=d['body'], metadata={
+            'title': d['title'],
+            'url': d['url']
+        }) for d in data])
+
+    vectorstore = Chroma.from_documents(docs, embedding=OpenAIEmbeddings())
+    print(vectorstore)
+
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(model_name="gpt-3.5-turbo-0613"),
+        memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True),
+        retriever=vectorstore.as_retriever(),
+        verbose=True,
+    )
+
+    while True:
+        try:
+            question = input("User: ")
+            resp = chain.run(question)
+            print('Agent:', resp)
+        except KeyboardInterrupt:
+            break
+
+
+if __name__ == '__main__':
+    main()
