0d66794e89a3ac307b135fd42109832702540c40
なるみ <toucans-cutouts0f@icloud.com>
2023-06-26 23:04:54 +0800

    feat: add MaiCoinFAQRetriever class to retrieve relevant documents
    - Add a new file `retriever.py` to `maicoinfaqbot` folder
    - Define a class `MaiCoinFAQRetriever` with various methods
    - Add a method `_run` that returns relevant documents for a given query
    - Add a method `_arun` that calls `_run` asynchronously
    - Add a class method `from_json` that reads from a JSON file and initializes `MaiCoinFAQRetriever`
    - Use `OpenAIEmbeddings` to initialize `Chroma` vectorstore in `from_json` method

---
+++ maicoinfaqbot/retriever.py
@@ -0,0 +1,38 @@
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.tools import BaseTool
+from langchain.vectorstores import Chroma
+from langchain.vectorstores.base import VectorStoreRetriever
+
+from .utils import load_json
+
+
+class MaiCoinFAQRetriever(BaseTool):
+    name: str = 'get_relevant_documents'
+    description: str = ('A MaiCoin FAQ article retriever.'
+                        'Input a query string.'
+                        'Output MaiCoin FAQ article(s) that are relevant to the query string.')
+
+    retriever: VectorStoreRetriever
+
+    def _run(self, query: str) -> str:
+        docs = self.retriever.get_relevant_documents(query)
+        return (f'Query: {query}\n'
+                f'Page Title: {doc.metadata["title"]}\n'
+                f'Page URL: {doc.metadata["url"]}\n'
+                f'Page Content: {doc.page_content}\n' for doc in docs)
+
+    async def _arun(self, query: str) -> str:
+        return self._run(query)
+
+    @classmethod
+    def from_json(cls, f: str):
+        data = load_json(f)
+        docs = RecursiveCharacterTextSplitter().split_documents(
+            [Document(page_content=d['body'], metadata={
+                'title': d['title'],
+                'url': d['url']
+            }) for d in data])
+        vectorstore = Chroma.from_documents(docs, embedding=OpenAIEmbeddings())
+        return cls(retriever=vectorstore.as_retriever())
