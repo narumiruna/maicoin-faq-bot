dfadb72a2961e0ddb97c44bb09e95051f070a281
narumi <toucans-cutouts0f@icloud.com>
2023-06-27 11:30:05 +0800

    feat: add logging statements for document processing
    - Import the `logger` module from `loguru`
    - Add logging statements for loading the json file, splitting documents, creating the vectorstore, and creating the chain

--- chain.py
+++ chain.py
@@ -6,6 +6,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
+from loguru import logger
 
 from maicoin_faq_bot.utils import load_json
 
@@ -13,17 +14,22 @@ from maicoin_faq_bot.utils import load_json
 def main():
     load_dotenv()
 
-    data = load_json('data/maicoin_faq_zh.json')
+    f = 'maicoin_faq_zh.json'
+    logger.info(f'Loading json file: {f}')
+    data = load_json(f)
 
+    logger.info('splitting documents...')
     docs = RecursiveCharacterTextSplitter().split_documents(
         [Document(page_content=d['body'], metadata={
             'title': d['title'],
             'url': d['url']
         }) for d in data])
 
+    logger.info('creating vectorstore...')
     vectorstore = Chroma.from_documents(docs, embedding=OpenAIEmbeddings())
     print(vectorstore)
 
+    logger.info('creating chain...')
     chain = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(model_name="gpt-3.5-turbo-0613"),
         memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True),
