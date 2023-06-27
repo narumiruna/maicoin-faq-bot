777365d8cfa1a11c11db8f88cb40d0f0167e7980
narumi <toucans-cutouts0f@icloud.com>
2023-06-27 11:41:48 +0800

    feat: add ConversationalRetrievalChain for MaiCoin FAQ bot
    - Add a new file `maicoin_faq_bot/chain.py`
    - Create class `MaiCoinFAQChain` with methods `__init__`, `from_env`, `create_chain`, and `run`
    - Load JSON file and split documents
    - Create vectorstore using Chroma and OpenAIEmbeddings
    - Use ChatOpenAI to create the llm
    - Create ConversationalRetrievalChain using llm, memory, and retriever
    - Allow the agent to run and respond to user input

---
+++ maicoin_faq_bot/chain.py
@@ -0,0 +1,69 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.base_language import BaseLanguageModel
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import BaseRetriever
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from loguru import logger
+
+from maicoin_faq_bot.utils import load_json
+
+
+class MaiCoinFAQChain:
+
+    def __init__(self, llm: BaseLanguageModel, retriever: BaseRetriever):
+        self.llm = llm
+        self.retriever = retriever
+
+        self.chains = {}
+
+    @classmethod
+    def from_env(cls):
+        model_name = os.environ('OPENAI_MODEL_NAME', 'gpt-3.5-turbo-0613')
+        llm = ChatOpenAI(model_name=model_name)
+
+        json_file = os.environ.get('MAICOIN_FAQ_JSON', 'maicoin_faq_zh.json')
+        logger.info(f'Loading json file: {json_file}')
+        data = load_json(json_file)
+
+        logger.info('splitting documents...')
+        docs = RecursiveCharacterTextSplitter().split_documents(
+            [Document(page_content=d['body'], metadata={
+                'title': d['title'],
+                'url': d['url']
+            }) for d in data])
+
+        logger.info('creating vectorstore...')
+        vectorstore = Chroma.from_documents(docs, embedding=OpenAIEmbeddings())
+
+        return cls(llm=llm, retriever=vectorstore.as_retriever())
+
+    def create_chain(self):
+        logger.info('creating memory...')
+        memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+        logger.info('creating chain...')
+        chain = ConversationalRetrievalChain.from_llm(
+            llm=self.llm,
+            memory=memory,
+            retriever=self.retriever,
+            verbose=True,
+        )
+
+        return chain
+
+    def run(self):
+        chain = self.create_chain()
+        while True:
+            try:
+                question = input("User: ")
+                resp = chain.run(question)
+                print('Agent:', resp)
+            except KeyboardInterrupt:
+                break
